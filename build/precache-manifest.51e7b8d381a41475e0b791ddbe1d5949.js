036b3e700ade3d3d056c3de5034e0b8f3b30c532
Elam <elamhe02@outlook.com>
2022-10-21 11:28:42 -0400

    adding all changes at once which includes package.json work and app src

---
+++ build/precache-manifest.51e7b8d381a41475e0b791ddbe1d5949.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0249038f4aa3f4adca31",
+    "url": "/elmhe/COMP3104/static/css/main.40bf2d55.chunk.css"
+  },
+  {
+    "revision": "0249038f4aa3f4adca31",
+    "url": "/elmhe/COMP3104/static/js/main.0249038f.chunk.js"
+  },
+  {
+    "revision": "42a88fd114019ab81186",
+    "url": "/elmhe/COMP3104/static/js/runtime~main.42a88fd1.js"
+  },
+  {
+    "revision": "b1773f8afd9fb6a0106e",
+    "url": "/elmhe/COMP3104/static/js/2.b1773f8a.chunk.js"
+  },
+  {
+    "revision": "e3e970e613908106a6768d498c9f31b9",
+    "url": "/elmhe/COMP3104/index.html"
+  }
+];
